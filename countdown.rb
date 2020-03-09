8fdefb37b47ae30fd59c440fb4a0fa293ef208ca
Charles Butler <cdbutle@outlook.com>
2020-03-09 04:20:44 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,13 +1,13 @@
 #write your code here
 
 def countdown
-  number = 10
-  until number == 10
-  puts "#{number} SECONDS!"
-  sleep 1
-  number -= 1
-end
-puts "HAPPY NEW YEAR!"
-end
+ number = 10
+ while number > 0
+ puts "#{number} SECONDS(S)!"
+ sleep 1
+ number -= 1
+ end
+ puts "HAPPY NEW YEAR!"
+ end
 
 countdown
