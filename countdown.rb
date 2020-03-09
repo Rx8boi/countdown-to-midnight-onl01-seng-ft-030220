b5691ef4dfa966565a3b9532c96264fd49246ff6
Charles Butler <cdbutle@outlook.com>
2020-03-09 04:38:58 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,6 +1,7 @@
 #write your code here
 
 def countdown(sec)
+  sec == 10
   while sec > 0
   puts "#{sec} SECOND(S)!"
   seconds -= 1
@@ -9,6 +10,7 @@ return "HAPPY NEW YEAR!"
 end
 
 def countdown_with_sleep(sec)
+  sec == 10
   while sec > 0
   puts "#{sec} SECOND(S)!"
   seconds -= 1
