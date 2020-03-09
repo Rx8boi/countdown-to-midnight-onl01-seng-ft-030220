8ba231c916cddc129adba81caabca84c0bb608b5
Charles Butler <cdbutle@outlook.com>
2020-03-09 04:22:00 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -11,3 +11,13 @@ def countdown
  end
 
 countdown
+
+def countdown_with_sleep
+   number = 10
+ while number > 0
+ puts "#{number} SECONDS(S)!"
+ sleep 1
+ number -= 1
+ end
+ puts "HAPPY NEW YEAR!"
+ end
