05364c8c913983c55bf8287fe40fba07dc556a67
Charles Butler <cdbutle@outlook.com>
2020-03-09 04:22:19 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -4,13 +4,10 @@ def countdown
  number = 10
  while number > 0
  puts "#{number} SECONDS(S)!"
- sleep 1
- number -= 1
+  number -= 1
  end
  puts "HAPPY NEW YEAR!"
  end
-
-countdown
 
 def countdown_with_sleep
    number = 10
